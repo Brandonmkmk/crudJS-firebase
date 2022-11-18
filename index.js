eb8f463906aba1659187d9f98717797496518f7b
Adrianxx1 <triaxis12q@gmail.com>
2022-11-17 18:34:29 -0600

    configuracion firebase con js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBhdvW4VCdEA8w3WNaVFVB7Z6ykpumOFn0",
+    authDomain: "fir-js-crud-f7baf.firebaseapp.com",
+    projectId: "fir-js-crud-f7baf",
+    storageBucket: "fir-js-crud-f7baf.appspot.com",
+    messagingSenderId: "29595613419",
+    appId: "1:29595613419:web:f442002fb154ebdf31a6ae"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
